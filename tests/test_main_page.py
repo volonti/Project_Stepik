63f920d362823f7e9e11ed1e3ee118d13f0f1d7c
Екатерина Сочкова <volontare@yandex.ru>
2023-02-04 16:06:33 +0400

    Test for main_page moved to tests

--- tests/test_main_page.py
+++ tests/test_main_page.py
@@ -1,16 +1,14 @@
-import time
-
 from pages.main_page import MainPage
 
 
-def test_guest_can_go_to_login_page(browser):
+def test_user_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
     page.open()
     page.go_to_login_page()
 
 
-def test_guest_should_see_login_link(browser):
+def test_user_should_see_login_link(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
     page.open()
